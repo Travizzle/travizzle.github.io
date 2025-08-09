b21062ecc11eb2157afcad04d3beaa09b748edb2
Travis Ireland <wesley@travisireland.com>
2025-08-09 12:01:48 -0600

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -2,8 +2,13 @@
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
 function activateGallery() {
+    // 2 different notations for nested selectors
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
     let mainImage = document.querySelector("#gallery-photo img");
+    // Image info to be updated
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title       = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
 
     thumbnails.forEach(function(thumbnail) {
         thumbnail.addEventListener("click", function() {
@@ -17,6 +22,10 @@ function activateGallery() {
             let currentClass = "current";
             document.querySelector(".current").classList.remove(currentClass);
             thumbnail.parentNode.classList.add(currentClass);
+
+            // Update image info.
+            title.innerHTML       = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
         });
     });
 }
